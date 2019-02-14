1da2bc2443bc773011e8319dac431a709afe462d
lequesneem <lequesneem@wgc.school.nz>
2019-02-15 10:01:58 +1300

    added fonts & colour to text and background

--- helloworld.py
+++ helloworld.py
@@ -6,15 +6,15 @@ root.title("hello world!")
 root.geometry("600x200")
 
 my_label = tkinter.Label(root)
-my_label.config(text="Hello!", fg="green")
+my_label.config(text="Emma!", fg="white", bg="orange", font=("nunito", "70", "italic"))
 my_label.grid()
 
 my_label = tkinter.Label(root)
-my_label.config(text="Hello there!", fg="blue")
+my_label.config(text="Emma!", fg="pink", bg="white", font=("Stencil", "70"), padx="20")
 my_label.grid()
 
 my_label = tkinter.Label(root)
-my_label.config(text="Hey you!", fg="pink")
+my_label.config(text="Emma!", fg="purple", bg="light blue", font=("Times", "70", "bold"), pady="30")
 my_label.grid()
 
 root.mainloop()
