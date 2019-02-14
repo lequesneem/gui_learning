6064268b7e98fc115701d59ec7b6a3e7a78dabf9
lequesneem <lequesneem@wgc.school.nz>
2019-02-14 15:14:37 +1300

    my_label added 3 paragraphs including colour & greetings

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
 import tkinter
+
 root = tkinter.Tk()
 
 root.title("hello world!")
@@ -6,6 +7,14 @@ root.geometry("600x200")
 
 my_label = tkinter.Label(root)
 my_label.config(text="Hello!", fg="green")
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="Hello there!", fg="blue")
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="Hey you!", fg="pink")
 my_label.grid()
 
 root.mainloop()
